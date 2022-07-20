c76e9946dc0634607960ecc3a21f297c49b9fd88
froggyai <108376328+froggyai@users.noreply.github.com>
2022-07-20 00:00:30 -0300

    Add files via upload

---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '63a4eb02cf924fdb9457e97d4a23129a';
+const topHeadlinesUrl =
+  'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
